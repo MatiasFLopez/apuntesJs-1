a4f3d77ed7b9216d87955dc56ec43ff03e3c49be
matiaslopez <matiaslopez-17@hotmail.com>
2022-12-11 19:38:50 -0300

    inicio del proyecto

---
+++ index.js
@@ -0,0 +1,12 @@
+// Formas de crear variables
+var miVariable = 24;
+console.log("mi edad es "+ miVariable)
+
+var miVariable = 14;
+console.log("mi edad ahora es "+ miVariable)
+
+const miConstante = 3;
+console.log("el valor de mi constante es "+ miConstante)
+
+// Operaciones con Variables
+
